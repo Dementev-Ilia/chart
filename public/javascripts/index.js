f912d456649e05f37ba7a875c4813d68c471d31a
Dementev Ilia <Dementew.ilia@yandex.ru>
2020-05-24 01:43:43 +0300

    create v1

--- public/javascripts/index.js
+++ public/javascripts/index.js
@@ -1,15 +1,36 @@
+(() => {
 console.log("socket.io");
 
-var socket = io();
-var form = document.getElementById('textField');
-var input = document.getElementById('m');
+let socket = io();
+let mesList = document.getElementById('message');
+let autorization = document.getElementById('autorization')
+let userName = document.getElementById('userName');
+let room = document.getElementById('room');
+let messageForm = document.getElementById('textField');
+let message = document.getElementById('m');
 
-
-
-form.onsubmit = function(e) {
+autorization.onsubmit = (e) => {
     e.preventDefault();
-    console.log(input.value);
-    socket.emit("chat message", input.value);
-    input.value = "";
+    console.log(userName.value);
+    console.log(room.value);
+    socket.emit("autorization", {user: userName.value, room: room.value});
+    userName.value = "";
+    room.value = "";
     return false;
 }
+
+messageForm.onsubmit = (e) => {
+    e.preventDefault();
+    console.log(message.value);
+    socket.emit("chat message", {msg: message.value});
+    message.value = "";
+    return false;
+}
+
+socket.on('chat message', (data) => {
+    mesList.append("<li>" + data.user + "</li>")
+})
+
+
+}
+)()
